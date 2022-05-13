091ae01d3f1f2152f15c14f12ac571658e1317e9
Khrystyna <tinalychko@gmail.com>
2022-05-13 15:18:56 +0300

    60

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Music from './Music/Music';
 import Settings from './Settings/Settings';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Friends from './NavBar/Friends/Friends';
-import { addPost } from './redux/state';
 import DialogsContainer from './Dialogs/DialogsContainer';
 import UsersContainer from './Users/UsersContainer';
 
@@ -24,8 +23,8 @@ const App = (props) => {
       <div className='app-wrapper-content'>
           <Routes>
             <Route path="/dialogs" element={<DialogsContainer store = {props.store}/>} />  
-            <Route path="/profile" element={<ProfileContainer 
-                    store = {props.store}/>} />
+            <Route path='/profile/:userId' element={<ProfileContainer />} />
+            {/* <Route path='/profile' element={<ProfileContainer />} /> */}
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
